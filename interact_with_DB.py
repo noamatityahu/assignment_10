27e4cad8a137e7885b9006ecb82475b01c7d0bd1
noamatityahu <73295862+noamatityahu@users.noreply.github.com>
2022-01-02 21:13:44 +0200

    Add files via upload

---
+++ interact_with_DB.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+def interact_db(query, query_type: str):
+    return_value = False
+    connection = mysql.connector.connect(host='localhost',
+                                         user='root',
+                                         passwd='root',
+                                         database='ex10')
+    cursor = connection.cursor(named_tuple=True)
+    cursor.execute(query)
+
+    if query_type == 'commit':
+        connection.commit()
+        return_value = True
+
+    if query_type == 'fetch':
+        query_result = cursor.fetchall()
+        return_value = query_result
+
+    connection.close()
+    cursor.close()
+    return return_value
+
